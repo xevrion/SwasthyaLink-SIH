1ad0bb791f462dda22179833615367538dc899dd
nayan9617 <nayanp4925@gmail.com>
2025-09-24 21:57:44 +0530

    minor config fixes

--- MVP/frontend/services/api.js
+++ MVP/frontend/services/api.js
@@ -8,9 +8,7 @@ const getBaseURL = () => {
   }
   if (Platform.OS === 'android') {
     // Use 10.0.2.2 for Android emulator, localhost for physical device
-    const BASE_URL = Platform.OS === 'android'
-      ? 'http://10.0.2.2:3000'
-      : 'http://localhost:3000';
+    return 'http://172.32.67.232:3000'; //use your laptop ip here
   }
   if (Platform.OS === 'ios') {
     return 'http://localhost:3000';
